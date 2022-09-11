d570aca9f4e2060edb6155aae3750039db62a0c3
pabhishekunofficial <pabhishekunofficial@gmail.com>
2022-09-11 10:39:08 +0530

    Initail Products Setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {Routes,Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import Header from './Components/Header';
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+import Footer from './Components/Footer';
 
 const App = ()=> {
   return (
-  <div className='App'>
-   <h1>Welcome To MyShop</h1>
-  </div>
+    <>
+    <Header />
+    <main className='py-3'> 
+      <Container>
+       <Routes>
+       <Route path='/' element={<HomeScreen />}/>
+       <Route path='/product/:id' element={<ProductScreen />} />
+      </Routes>
+      </Container>  
+    </main>
+    <Footer />
+  </>
 
   );
 }

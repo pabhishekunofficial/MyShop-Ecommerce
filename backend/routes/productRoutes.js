4025735bbcdd000a7606842290ff9506fb81eb4d
pabhishekunofficial <pabhishekunofficial@gmail.com>
2022-09-21 23:56:33 +0530

    added json web token functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,24 +1,12 @@
 import express from "express";
-import Product from "../models/productModel.js";
-import asyncHandler from 'express-async-handler'
+import { getProducts, getProductById } from "../controllers/productControllers.js";
 
 const router = express.Router()
 
-router.get('/', asyncHandler(async(req,res)=>{
-   const products = await Product.find({})
+// router.get('/', getProducts)
+router.route('/').get(getProducts);
 
-    res.json(products)
-}))
-
-router.get('/:id', asyncHandler(async(req,res)=>{
-    const product = await Product.findById(req.params.id)
-
-    if(product){
-      res.json(product)
-   } else{
-    res.status(404)
-    throw new Error('Product not found')
-   }
-}))
+// router.get('/:id', getProductById)
+router.route('/:id').get(getProductById)
 
 export default router

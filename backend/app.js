3bcde31687d421bce2226f9ce01085d3a99d1a94
pabhishekunofficial <pabhishekunofficial@gmail.com>
2022-10-01 00:21:16 +0530

    some update

--- backend/app.js
+++ backend/app.js
@@ -35,7 +35,7 @@ app.get('/api/config/paypal', (req,res) => {
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
-if(process.env.NODE_ENV === 'production') {
+if(true) {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
     app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
